50628e79f16ab995d078327962c2306fe1fdaf2e
Mark <mark94e@yahoo.com>
2015-10-07 16:52:50 -0400

    Setup

---
+++ app/src/main/java/com/cse/one4all/One4All.java
@@ -0,0 +1,34 @@
+package com.cse.one4all;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import java.io.IOException;
+
+public class One4All extends SimpleBaseGameActivity {
+
+    private static int CAMERA_WIDTH = 800;
+    private static int CAMERA_HEIGHT = 480;
+
+    @Override
+    protected void onCreateResources() throws IOException {
+
+    }
+
+    @Override
+    protected Scene onCreateScene() {
+        final Scene scene = new Scene();
+
+        return scene;
+    }
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+    }
+}
