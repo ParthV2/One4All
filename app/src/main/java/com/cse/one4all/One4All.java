2aa7796a8a244c5d80cbff7f69fafe8c2b4d8dfe
Mark <mark94e@yahoo.com>
2015-10-13 11:24:08 -0400

    added andengine physics extension

--- app/src/main/java/com/cse/one4all/One4All.java
+++ app/src/main/java/com/cse/one4all/One4All.java
@@ -5,30 +5,35 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.ui.IGameInterface;
+import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
 import java.io.IOException;
 
-public class One4All extends SimpleBaseGameActivity {
+public class One4All extends BaseGameActivity {
 
     private static int CAMERA_WIDTH = 800;
     private static int CAMERA_HEIGHT = 480;
 
     @Override
-    protected void onCreateResources() throws IOException {
-
-    }
-
-    @Override
-    protected Scene onCreateScene() {
-        final Scene scene = new Scene();
-        //
-        return scene;
-    }
-
-    @Override
     public EngineOptions onCreateEngineOptions() {
         final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
         return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws IOException {
+
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws IOException {
+
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException {
+
     }
 }
