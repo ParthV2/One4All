67eca89c8dbda1fd6a3e621fdcf0bc20f2497f23
ColePillars <cole.pillars@e7solutions.com>
2015-10-27 11:35:05 -0400

    Added Obstacle (no hit detection)

--- app/src/main/java/com/cse/one4all/minigame/Helicopter.java
+++ app/src/main/java/com/cse/one4all/minigame/Helicopter.java
@@ -15,15 +15,29 @@ import org.andengine.input.touch.TouchEvent;
 public class Helicopter extends BaseMinigame {
     private Text helicopter;
     private Text thrust;
+    private Text obstacle1;
 
-    private float heliPos = 0;
-    private float heliVel = 0;
+    private float heliPos;
+    private float heliVel;
     private final float heliAcc = -1;
+
+    private float obstacle1XPos;
+    private float obstacle1YPos;
+    private float obstacle1Vel;
 
     public void updateHeli()
     {
         heliPos = heliPos + heliVel;
         heliVel = heliVel + heliAcc;
+    }
+
+    public void updateObstacles()
+    {
+        if (obstacle1XPos < -400 ) {
+            obstacle1XPos = 400;
+        } else {
+            obstacle1XPos = obstacle1XPos + obstacle1Vel;
+        }
     }
 
     public void applyThrust()
@@ -34,16 +48,28 @@ public class Helicopter extends BaseMinigame {
     @Override
     public void onStart()
     {
+        heliPos = 0;
+        heliVel = 0;
+        obstacle1XPos = 400;
+        obstacle1YPos = 100;
+        obstacle1Vel = -10;
         engine.registerUpdateHandler(new TimerHandler(0.1f, new ITimerCallback()
         {
             public void onTimePassed(final TimerHandler pTimerHandler)
             {
                 //Updates Helicopters Position and Velocity
                 updateHeli();
+                updateObstacles();
                 helicopter.setPosition(camera.getCenterX(), camera.getCenterY() + heliPos);
+                obstacle1.setPosition(camera.getCenterX() + obstacle1XPos, camera.getCenterY() + obstacle1YPos);
+
+                if( heliPos >= camera.getCenterY() || heliPos <= camera.getCenterY() - 450 ) {
+                    complete();
+                }
 
                 //Stops when helicopter hits the top or bottom of the screen
-                if(heliPos >= camera.getCenterY() || heliPos <= camera.getCenterY() - 450 ){
+                if(completed)
+                {
                     engine.unregisterUpdateHandler(pTimerHandler);
                 } else {
                     pTimerHandler.reset();
@@ -67,9 +93,12 @@ public class Helicopter extends BaseMinigame {
         };
 
         //Helicopter Image
-        helicopter = new Text(camera.getCenterX(), camera.getCenterY() + heliPos, ResourcesManager.getInstance().font, "Helicopter", scene.vbom);
+        helicopter = new Text(camera.getCenterX(), camera.getCenterY() + heliPos, ResourcesManager.getInstance().font, "Heli", scene.vbom);
+
+        obstacle1 = new Text(camera.getCenterX() + obstacle1XPos, camera.getCenterY() + obstacle1YPos, ResourcesManager.getInstance().font, "GG", scene.vbom);
 
         scene.attachChild(helicopter);
+        scene.attachChild(obstacle1);
         scene.attachChild(thrust);
         scene.registerTouchArea(thrust);
     }
